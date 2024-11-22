8fca105e354aeb4873d52e7a1d9820b960618a7b
IlseChan <judai,chan@gmail.com>
2024-11-22 16:39:04 -0600

    modificaciones en main.ts y en env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config/envs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost', 
+      port: envs.port, 
+    },
+  });
+
+  console.log(`Microservicio RVIAAC corriendo en localhost:${envs.port}`);
+  await app.listen();
 }
+
 bootstrap();
